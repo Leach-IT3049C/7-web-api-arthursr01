af1fd95f06ade289aac876516ec5ab0c403fa9cb
arthursr01 <70337560+arthursr01@users.noreply.github.com>
2020-10-11 15:20:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-window.onload = function() {
+window.onclick = function() {
 var constraints =  { audio: true,
                      video: true};
+
 navigator.mediaDevices.getUserMedia(constraints).then(function(mediaStream) {
     var video = document.querySelector('video');
     video.srcObject = mediaStream;
     video.play();
+  
 }).catch(function(err){
     console.log('Unexpected Error.' + err.message)
 })
